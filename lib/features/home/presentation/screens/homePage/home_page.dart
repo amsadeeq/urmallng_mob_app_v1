978c929fd50ad62894c2d2982630f1db0a0073cc
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2026-03-28 22:44:40 +0800

    after updating flutter dart and package

--- lib/features/home/presentation/screens/homePage/home_page.dart
+++ lib/features/home/presentation/screens/homePage/home_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:urmallng_mob_app_v1/features/home/presentation/screens/homePage/widgets/campaign_carousel_widget.dart';
 import 'package:urmallng_mob_app_v1/features/home/presentation/screens/homePage/widgets/categories_widget.dart';
-import 'package:urmallng_mob_app_v1/features/home/presentation/screens/homePage/widgets/products_widget.dart';
 import 'package:urmallng_mob_app_v1/features/home/presentation/screens/homePage/widgets/search_bar_widget.dart';
 import 'package:urmallng_mob_app_v1/features/home/presentation/widgets/product_carousel.dart';
-import 'package:urmallng_mob_app_v1/shared/styles/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
